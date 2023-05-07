88b40c53ff054399df8fe48ef9bb18b58612ea38
shilpa <mulgeshilpa@gmail.com>
2023-05-07 12:01:03 +0530

    request and response

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  const url = req.url;
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Node</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Node</title></head>");
+    res.write("<body><h1>Welcome About us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Node</title></head>");
+    res.write("<body><h1> Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Node</title></head>");
