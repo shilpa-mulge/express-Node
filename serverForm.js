9113b028d30d848b1558cb09e7b43d47570c2880
shilpa <mulgeshilpa@gmail.com>
2023-05-07 15:39:22 +0530

    Writing and reading from a file

--- serverForm.js
+++ serverForm.js
@@ -2,26 +2,40 @@ const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
+    fs.readFile("./message.txt", (err, data) => {
+      const content = data.toString();
+      res.write("<html>");
+      res.write("<head><title>my node</title></head>");
+      res.write(`<body>${content}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = "302";
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
     res.write("<html>");
     res.write("<head><title>my node</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body>'
-    );
+    res.write("<body><h1>hello</h1></body>");
     res.write("</html>");
-    return res.end();
+    res.end();
   }
-  if (url === "/message") {
-    fs.writeFileSync("message.txt", "dummy");
-    res.statusCode = "302";
-    res.setHeader("location", "/");
-    return res.end();
-  }
-  res.write("<html>");
-  res.write("<head><title>my node</title></head>");
-  res.write("<body><h1>hello</h1></body>");
-  res.write("</html>");
-  return res.end();
 });
 
 server.listen(4000);
