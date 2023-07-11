beade83a57bb456c7e21b9f48433b9101a6df0bf
shilpa <mulgeshilpa@gmail.com>
2023-07-11 16:21:35 +0530

    middelware commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("in the middleware!");
-  next();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-pro", (req, res, next) => {
+  console.log("The add product page!");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' placeholder='product name'><input type='number' name='size' placeholder='product size'><button type='submit'>add pro</button></form>"
+  );
 });
-app.use((req, res, next) => {
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
   console.log("in the another middleware!");
   res.send("<h1>Hello from express!</h1>");
 });
